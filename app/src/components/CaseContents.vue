45c8809e104daea3cfd644ee84cef639448db429
Holiday <holidayexplanation@gmail.com>
2021-01-27 21:26:11 +0100

    *

--- app/src/components/CaseContents.vue
+++ app/src/components/CaseContents.vue
@@ -1,14 +1,21 @@
 <template>
   <div>
     <ul>
-      <li v-for="gun in guns" :key="gun">
-        <p>{{ gun }}</p>
+      <li v-for="(gun, i) in guns" :key="i">
+        <img :src="require(`@/assets/skins/${guns[i]}.png`)" alt="">
+        <p class="skinTitle">{{ gun }}</p>
+        <button id="Inspect" @click="inspectGun(gun)">
+          Inspect in-game (FN)
+        </button>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+// const steamprice = require('steam-price-api');
+
 export default {
   name: 'CaseContents',
   props: ['Case'],
@@ -17,18 +24,49 @@ export default {
       guns: [] // arr of gun names
     }
   },
-  mounted() {
+  // trading url
+  // https://steamcommunity.com/my/tradeoffers/privacy#trade_offer_access_url
+  async mounted() {
     console.log(this.Case)
 
     this.importGunData()
+    axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 
+  //   fetch('http://steamcommunity.com/market/priceoverview/?currency=3&appid=730&market_hash_name=StatTrak%E2%84%A2%20P250%20%7C%20Steel%20Disruption%20%28Factory%20New%29')
+  // .then(response => response.json())
+  // .then(json => console.log(json))
+    //For one item
+    // const data = await steamprice.getprice(730, "Clutch Case", 1)
+    // console.log(data)
+
+    // const items = [
+    // 'Clutch Case Key',
+    // 'Clutch Case'
+    // ]
+    // steamprice.getprices(730, items, '1').then(data => {
+    //     console.log(data);
+    // }).catch(err => console.log(err));
+
+     
+
+     const data = await axios.get(
+       'http://steamcommunity.com/market/priceoverview/?currency=3&appid=730&market_hash_name=StatTrak%E2%84%A2%20P250%20%7C%20Steel%20Disruption%20%28Factory%20New%29'
+     )
+
+     console.log(data)
   },
   methods: {
+    inspectGun(gun) {
+      console.log(gun)
+      const gun_id = "20M3462576415354083999A13819871659D16352124405222944812"
+      const link = `steam://rungame/730/76561202255233023/+csgo_econ_action_preview%${gun_id}`
+
+      window.open(link)
+    },
     importGunData() {
       switch (this.Case) {
         case 'clutch':
             this.guns = require(`@/assets/gunData/${this.Case}.json`)
-            console.log(this.guns)
           break;
       
         default:
@@ -40,5 +78,54 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '@/assets/mixins/centerX';
+@import '@/assets/mixins/centerY';
+@import '@/assets/mixins/centerXY';
+@import '@/assets/mixins/unselectable';
+
+div {
+  height: 70vh;
+  width: 100vw;
+}
+
+ul {
+  @include centerXY;
+  // border: 1px solid white;
+  width: 90%;
+  height: 90%;
+  overflow-y: auto;
+  li {
+    float: left;
+    margin: 15px;
+    height: 250px;
+    width: 250px;
+    border: 2px solid rgba(255, 255, 255, 0.2);
+    background-color: rgba(255, 255, 255, 0.05);
+    border-radius: 10px;
+    img {
+      margin-top: 20px;
+      height: 120px;
+    }
+    p {
+      font-weight: bold;
+    }
+    #Inspect {
+      margin-top: 15px;
+      width: 90%;
+      padding: 5px 0 5px 0;
+      border: none;
+      border-radius: 5px;
+      color: whitesmoke;
+      background: -webkit-linear-gradient(#484e55,#3a3f44 60%,#313539);
+      box-shadow: 2px 2px 6px 1px rgba(0, 0, 0, 0.2);
+      cursor: pointer;
+      &:hover {
+        transition: .15s ease;
+        transform: scale(1.01);
+        background: -webkit-linear-gradient(#337bce,#0b5aa8 60%,#65a9ee);
+      }
+    }
+  }
+}
 
 </style>
