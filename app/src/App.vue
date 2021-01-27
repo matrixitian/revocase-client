783c226356e4563ac0cf6b849601629aab96fcbc
Holiday <holidayexplanation@gmail.com>
2021-01-27 12:04:46 +0100

    *

--- app/src/App.vue
+++ app/src/App.vue
@@ -3,13 +3,29 @@
 
     <div id="Topper">
       <div id="Left">x</div>
-      <div id="Middle">c</div>
+      <div id="Middle">
+        <div id="centerLogo">
+          <div id="centerUp">
+            <img id="Logo" src="@/assets/logo.svg" alt="RevoSkins.Eu Logo">
+            <p id="logoText">Revo Skins</p>
+          </div>
+          <div id="centerDown">
+            <p id="logoInfo">Watch Ads in Background while playing CS and earn up to 5$ a day in points!</p>
+          </div>
+        </div>
+      </div>
       <div id="Right">z</div>
     </div>
 
     <div id="Bottomer">
       <div id="Upper">x</div>
-      <div id="Downer">u</div>
+      <div id="Downer">
+        <ul>
+          <li v-for="(crate, i) in crates" :key="i">
+
+          </li>
+        </ul>
+      </div>
     </div>
 
     <!-- <img id="Logo" alt="RevoCase.eu logo" src="./assets/logo.svg">
@@ -24,32 +40,111 @@ export default {
   name: 'App',
   components: {
     // HelloWorld
+  },
+  data() {
+    return {
+      crates: ['Bravo', 'Test2', 'Test3',
+      'Test4', 'Test5', 'Test6']
+    }
   }
 }
 </script>
 
 <style lang="scss">
+@font-face {
+  font-family: SoleilExtraBold;
+  src: url('./assets/fonts/soleil_extrabold.ttf');
+}
+@import '@/assets/mixins/centerY';
+@import '@/assets/mixins/centerXY';
+
+$grayBackground: #1b2435;
+$redGradientStart: #ea5a8d;
+$redGradientEnd: #e32565;
+$orangeGradientStart: #e32565;
+$orangeGradientEnd: #ff7b36;
+$yellowGradientStart: #ff7b36;
+$yellowGradientEnd: #ffcc01;
+$greenGradientStart: #6bc498;
+$greenGradientEnd: #2fad6e;
+$blueGradientStart: #4ba8b7;
+$blueGradientEnd: #018498;
+$purpleGradientStart: #7967bb;
+$purpleGradientEnd: #5a43ab;
+
+body {
+  background-color: $grayBackground;
+}
+
+#logoInfo {
+  position: absolute;
+  bottom: 0;
+}
+
+#app {
+  font-family: 'SoleilExtraBold', Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: whitesmoke;
+  text-align: center;
+  grid-template-columns: 200px auto 200px;
+  grid-template-rows: 300px 120px auto;
+  height: 100vh;
+}
+
+#centerLogo {
+  @include centerXY;
+  background-color: greenyellow;
+  width: 400px;
+  height: 150px;
+  margin: auto;
+  #centerUp {
+    background-color: hotpink;
+    height: 70%;
+    position: relative;
+    #Logo {
+      @include centerY;
+      left: 0;
+      height: 100px;
+      width: 100px;
+    }
+    #logoText {
+      @include centerY;
+      position: absolute;
+      right: 0;
+      font-weight: bold;
+      font-size: 47px;
+    }
+  }
+}
+
+$leftTopperWidth: 150px;
+$rightTopperWidth: 150px;
+$middleTopperWidth: calc(100% - #{$leftTopperWidth} - #{$rightTopperWidth});
 
 #Topper {
   background-color: #018498;
   width: 100vw;
   height: 30vh;
   #Left {
-    width: 10vw;
+    width: $leftTopperWidth;
     height: 100%;
     background-color: #e32565;
     display: inline;
     float: left;
   }
   #Middle {
-    width: 80%;
+    position: relative;
+    width: $middleTopperWidth;
     height: 100%;
     background-color: #802a46;
     display: inline;
     float: left;
+    display: table;
   }
   #Right {
-    width: 10%;
+    width: $rightTopperWidth;
     height: 100%;
     background-color: #259de3;
     display: inline;
@@ -72,41 +167,6 @@ export default {
     width: 100%;
     height: calc(100% - 100px);
   }
-}
-
-$grayBackground: #1b2435;
-$redGradientStart: #ea5a8d;
-$redGradientEnd: #e32565;
-$orangeGradientStart: #e32565;
-$orangeGradientEnd: #ff7b36;
-$yellowGradientStart: #ff7b36;
-$yellowGradientEnd: #ffcc01;
-$greenGradientStart: #6bc498;
-$greenGradientEnd: #2fad6e;
-$blueGradientStart: #4ba8b7;
-$blueGradientEnd: #018498;
-$purpleGradientStart: #7967bb;
-$purpleGradientEnd: #5a43ab;
-
-body {
-  background-color: $grayBackground;
-}
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: whitesmoke;
-  text-align: center;
-  grid-template-columns: 200px auto 200px;
-  grid-template-rows: 300px 120px auto;
-  height: 100vh;
-}
-
-#Logo {
-  height: 100px;
-  width: 100px;
 }
 
 button, input {
